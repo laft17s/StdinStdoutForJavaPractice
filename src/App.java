0ee0c083525045120751362b58c81ef9f2908350
Sean17nl <sean17nl@gmail.com>
2021-12-11 22:31:42 -0500

    start project

---
+++ src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /**
+         * @title: Stdin & Stdout Practice
+         * 
+         *         ## Excercise:
+         *         Receive an integer three times per keyboard and then print the three
+         *         values.
+         * 
+         *         ## Example:
+         * 
+         *         ### Input
+         *         >> 10
+         *         >> 23
+         *         >> 75
+         * 
+         *         ### Output
+         *         >> 10
+         *         >> 23
+         *         >> 75
+         */
+    }
+}
